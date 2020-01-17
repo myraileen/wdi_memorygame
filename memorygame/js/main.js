11e398108484020a18468d7f8045c522e4c36bb1
Fah Myra <mf12629@wdxmf12629vcg.90.deere.com>
2020-01-17 12:01:50 -0600

    Added game logic.

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,9 +1,16 @@
 console.log("Up and running!");
 
-cards = ["queen","queen","king","king"];
-cardsInPlay = [];
+//create array to store cards
+let cards = ["queen","queen","king","king"];
 
+//variable arrow for cards in play
+let cardsInPlay = [];
+
+//variable for first card the user picks
 let cardOne = cards[0];
 
+//add first card picked to cards in play array
 cardsInPlay.push(cardOne);
+
+console.log("User flipped queen.")
 
