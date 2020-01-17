2f6f8fee9db372896f6aedd36b43cc8b5cb7315f
Fah Myra <mf12629@wdxmf12629vcg.90.deere.com>
2020-01-17 13:03:59 -0600

    updated game logic with functions.

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,16 +1,33 @@
 console.log("Up and running!");
 
-//create array to store cards
+//create array varables to store cards available and cards in play
 let cards = ["queen","queen","king","king"];
-
-//variable arrow for cards in play
 let cardsInPlay = [];
 
-//variable for first card the user picks
-let cardOne = cards[0];
+//function to check for matching cards in play
+function checkForMatch () {
+	  if (cardsInPlay[0] === cardsInPlay[1]) {
+	  console.log("You found a match!");
+	} else {
+	  console.log("Sorry, try again.");
+	}
+}
 
-//add first card picked to cards in play array
-cardsInPlay.push(cardOne);
+//flip card function
+function flipCard (cardId) {
+	cardsInPlay.push(cards[cardId])
+	console.log("User flipped " + cards[cardId] + ".")
+    checkForMatch();
 
-console.log("User flipped queen.")
+	//variable for first card the user picks
+	//let cardOne = cards[0];
+	//add first card picked to cards in play array
+	//cardsInPlay.push(cardOne);
+
+}
+
+flipCard(0);
+flipCard(3);
+	
+
 
